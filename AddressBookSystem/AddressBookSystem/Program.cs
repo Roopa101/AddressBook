5b6dbf763f51f0972448b9257ff6d7682174ee7b
Roopa Jamadar <roopajamadar14@gmail.com>
2021-12-02 20:55:40 +0530

    [Roopa] Deleted A Person using Person's Name

--- AddressBookSystem/AddressBookSystem/Program.cs
+++ AddressBookSystem/AddressBookSystem/Program.cs
@@ -10,65 +10,55 @@ namespace AddressBookSystem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Address Book Program");
+            bool flag = true;
+            while (flag)
             {
-                bool flag = true;
-                Contact contact = new Contact();
-                AddressBook addressBook = new AddressBook();
-                while (flag)
+                Console.WriteLine("Welcome to Address Book Program");
+                Console.WriteLine("Enter what you want to do : \n 1. Create Contacts \n 2. Add Contacts \n 3. Edit Contact \n 4. Delete Contact \n 5. Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
                 {
-                    Console.WriteLine("Welcome to the Address Book Program");
-                    Console.WriteLine("Enter what you want to do : \n 1. Create Contacts \n 2. Add Contacts \n 3. Edit Contacts \n 4. Exit");
-                    int option = Convert.ToInt32(Console.ReadLine());
-                    switch (option)
-                    {
-                        case 1:
-                            Console.WriteLine("Enter the Contact details of FirstName, LastName, Address, City, State, Zip, Ph.no, Email : ");
-                            contact = new Contact()
-                            {
-                                FirstName = Console.ReadLine(),
-                                LastName = Console.ReadLine(),
-                                Address = Console.ReadLine(),
-                                City = Console.ReadLine(),
-                                State = Console.ReadLine(),
-                                Zip =Convert.ToInt32( Console.ReadLine()),
-                                MobileNumber = Convert.ToDouble( Console.ReadLine()),
-                                Email = Console.ReadLine(),
-                            };
-                            break;
-                        
-                        case 2:
-                            Console.WriteLine("Enter the Contact Information to Add, in form of FirstName, LastName, Addr, City, State, Zip, Ph.No, Email.");
-                            contact = new Contact()
-                            {
-                                FirstName = Console.ReadLine(),
-                                LastName = Console.ReadLine(),
-                                Address = Console.ReadLine(),
-                                City = Console.ReadLine(),
-                                State = Console.ReadLine(),
-                                Zip = Convert.ToInt32(Console.ReadLine()),
-                                MobileNumber = Convert.ToDouble(Console.ReadLine()),
-                                Email = Console.ReadLine(),
-                            };
-                            
-                            addressBook.AddContact(contact);
-                            addressBook.Display();
-                            break;
-                        
-                        case 3:
-                            Console.WriteLine("Enter the Contact Name to  Edit: ");
-                            string name = Console.ReadLine();
-                            addressBook.EditContact(name);
-                            Console.WriteLine("Contact is Edited Sucsessfully");
-                            addressBook.Display();
-                            break;
-                        case 4:
-                            flag = false;
-                            break;
-                    }
-                    Console.ReadLine();
+                    case 1:
+                        Console.WriteLine("Enter the Contact details of FirstName, LastName, Address, City, State, Zip, Ph.no, Email");
+                        ContactBook contact = new ContactBook()
+                        {
+                            FirstName = Console.ReadLine(),
+                            LastName = Console.ReadLine(),
+                            Address = Console.ReadLine(),
+                            City = Console.ReadLine(),
+                            State = Console.ReadLine(),
+                            Zip = Console.ReadLine(),
+                            PhoneNumber = Console.ReadLine(),
+                            Email = Console.ReadLine(),
+                        };
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter the contact to be added : ");
+                        ContactBook con = new ContactBook();
+                        con.AddContact();
+                        con.Display();
+                        break;
+                    case 3:
+                        Console.WriteLine("Enter the contact which you want to Edit");
+                        string name = Console.ReadLine();
+                        ContactBook edit = new ContactBook();
+                        edit.AddContact();
+                        edit.EditContact(name);
+                        edit.Display();
+                        break;
+                    case 4:
+                        Console.WriteLine("Enter the contact you want to delete :");
+                        string detail = Console.ReadLine();
+                        ContactBook delete = new ContactBook();
+                        delete.AddContact();
+                        delete.DeleteContact(detail);
+                        delete.Display();
+                        break;
+                    case 5:
+                        flag = false;
+                        break;
                 }
             }
         }
     }
-}    
+}
