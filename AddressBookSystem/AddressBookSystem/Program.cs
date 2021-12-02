bdc677b703e88308d880d2d2ff76f96302dd4fe3
Roopa Jamadar <roopajamadar14@gmail.com>
2021-12-02 19:34:30 +0530

    [Roopa]Created Contacts in Address Book

--- AddressBookSystem/AddressBookSystem/Program.cs
+++ AddressBookSystem/AddressBookSystem/Program.cs
@@ -11,7 +11,36 @@ namespace AddressBookSystem
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address Book Program");
-            Console.ReadLine();
+            {
+                bool flag = true;
+                while (flag)
+                {
+                    Console.WriteLine("Welcome to Address Book Program");
+                    Console.WriteLine("Enter what you want to do : \n 1. Create Contacts \n 2. Exit");
+                    int option = Convert.ToInt32(Console.ReadLine());
+                    switch (option)
+                    {
+                        case 1:
+                            Console.WriteLine("Enter the Contact details of FirstName, LastName, Address, City, State, Zip, Ph.no, Email");
+                            ContactBook contact = new ContactBook()
+                            {
+                                FirstName = Console.ReadLine(),
+                                LastName = Console.ReadLine(),
+                                Address = Console.ReadLine(),
+                                City = Console.ReadLine(),
+                                State = Console.ReadLine(),
+                                Zip = Convert.ToInt32( Console.ReadLine()),
+                                MobileNumber = Convert.ToDouble( Console.ReadLine()),
+                                Email = Console.ReadLine(),
+                            };
+                            break;
+                        case 2:
+                            flag = false;
+                            break;
+                    }
+                }
+            }
         }
     }
 }
+    
